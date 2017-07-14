fd205f2bfd165df88f7aa75e606de707624af91a
eavillagran <eavillagran@uc.cl>
2017-07-14 16:48:38 -0400

    actualizar db

--- db.py
+++ db.py
@@ -1,10 +1,16 @@
 import psycopg2 as psql
 
 dbg = "[DEBUG] "
+
 class DB():
 
     def __init__(self):     
-        self.conn = psql.connect("dbname=datospsu user=felipe password=123abc456 host=localhost port=5432")
+        self.conn = psql.connect("""
+                dbname=datospsu 
+                user=felipe 
+                password=123abc456 
+                host=localhost 
+                port=5432""")
         self.cur = self.conn.cursor()
         self.cur_for = self.conn.cursor()
 
@@ -14,29 +20,45 @@ class DB():
             self.cur.execute(query, args)
             self.conn.commit()
         except Exception as e:
+            # Exception don felipe
             print(dbg, "Error al hacer la consulta: ", query, "\n", "Error: ", e)
             raise Exception("Se cashooooo boludo")
         return
 
     def createTables(self):
-        self.cur.execute("DROP TABLE IF EXISTS students, scores")
+        # Eliminar tablas
+        self.cur.execute("DROP TABLE IF EXISTS students, scores, postulaciones")
         self.conn.commit()
-        self.exec("""CREATE TABLE students(id serial primary key, rut text, name text)""")
-        self.exec("""CREATE TABLE scores(id serial primary key,
-                                         id_student int references students(id),
-                                         mat int, len int, cie int,
-                                          his int, nem int)""")
+        # Crear tabla estudiantes
+        self.exec("""
+            CREATE TABLE students(id serial primary key, 
+            rut text, name text, carrera text, universidad text)""")
+        # Crear tabla puntajes
+        self.exec("""
+            CREATE TABLE scores(id serial primary key,
+            id_student int references students(id),
+            mat int, len int, cie int, his int, nem int)""")
 
     def insertStudents(self, filename):
+        """ 
+        InsertStudents: metodo para ir subiendo a los usuarios. Lee el 
+        nombre del archivo y va recorriendo el archivo.
+        """
+        # Agregar los estudiantes a la base de datos
         with open(filename+".csv", "r") as reader:
             reader.readline()
             for linea in reader:
                 linea = linea.split(",")
-                # print(linea)
-                self.exec("INSERT INTO students(rut, name) VALUES(%s,%s)", linea[0], linea[1])
-
+                # Agregar los alumnos con rut, nombre, carrera y universidad
+                self.exec("""
+                    INSERT INTO students(rut, name, carrera, universidad) 
+                    VALUES(%s,%s)""", linea[0], linea[1], linea[5], linea[4])
 
     def insertScore(self, ide, puntajes):
+        """ 
+        InsertScore: Metodo para subir los puntajes a la base de datos. Recibe 
+        el id del usuario y los puntajes y lo sube.
+        """
         id_student = ide
         leng = int(puntajes[0])
         mat = int(puntajes[1])
@@ -44,8 +66,10 @@ class DB():
         his = int(puntajes[3])
         nem = int(puntajes[4])
 
-        self.exec("INSERT INTO scores(id_student, mat, len, cie, his, nem) VALUES(%s,%s,%s,%s,%s,%s)", id_student, leng, mat, cie, his, nem)
-
+        self.exec("""
+            INSERT INTO scores(id_student, mat, len, cie, his, nem) 
+            VALUES(%s,%s,%s,%s,%s,%s)""", 
+            id_student, leng, mat, cie, his, nem)
 
 
 if __name__ == "__main__":
@@ -53,4 +77,3 @@ if __name__ == "__main__":
     db.createTables()
     db.insertStudents("usuarios")
     db.conn.close()
-
--- main.py
+++ main.py
@@ -3,18 +3,24 @@ import db as DB
 
 if __name__  == "__main__":
     db = DB.DB()
+    # Obtener el ultimo usuario que se subio
     db.cur.execute("SELECT max(id_student) FROM scores;")
     max_id = db.cur.fetchone()[0]
+    # Si hay ultimo usuario se fija en ese
     if max_id:
     	db.cur_for.execute("SELECT id,rut FROM students WHERE id > %s;", (max_id,))
+    # Si no hay ultimo usuario se executa desde cero
     else:
     	db.cur_for.execute("SELECT id,rut FROM students;")
 
+    # Para cada alumno en la consulta
     for alumno in db.cur_for:
         ide = alumno[0]
         rut = alumno[1]
         print(DB.dbg, "ID alumno: {} - RUT: {}".format(ide,rut))
+        print(DB.dbg, 'Carrera: {} - Universidad: {}'.format(alumno[2], alumno[3]))
         try:
+            # Subir los puntajes
         	ptjes = functions.getInfo(rut)
         	db.insertScore(ide, ptjes)
         except Exception as e:
