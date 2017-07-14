ba8725ed5a46f29540f60741207b558d3f9d4cc1
eavillagran <eavillagran@uc.cl>
2017-07-14 18:24:23 -0400

    alumnos peseu 2014

---
+++ usuarios_peseu_2014.py
@@ -0,0 +1,83 @@
+from lxml import html
+import requests
+import csv
+
+# Obtener la lista de universidades
+pagina = requests.get('http://www.peseu.com/listas-de-seleccionados/2014/')
+tree_ues = html.fromstring(pagina.content)
+ues = tree_ues.xpath('//tbody/tr/td/a/text()')[::2]
+
+# Acá se guardaran las universidades con sus carreras
+universidades_con_carreras = {}
+
+# Iterar sobre las universidades para sacar las carreras
+for u in ues:
+    print('[DEBUG] Obteniendo carreras de: {}'.format(u))
+    pagina_carreras = requests.get(
+        'http://www.peseu.com/listas-de-seleccionados/2014/{}'.format(u))
+    tree_carreras = html.fromstring(pagina_carreras.content)
+
+    nombres_carreras = tree_carreras.xpath('//table/tr/td/text()')[::2]
+    codigos_carreras = tree_carreras.xpath('//table/tr/td/a/text()')[::2]
+
+    # Guardar las carreras con sus codigos en una tupla
+    universidades_con_carreras[u] = list(
+        zip(codigos_carreras, nombres_carreras))
+
+# Datos guardados de las personas
+personas_con_carreras = {}
+
+# Cookies guardadas para hacer request
+cookies = {
+    '__utmt': '1',
+    '__utma': '17573176.466661072.1498867554.1498867554.1498870955.2',
+    '__utmb': '17573176.111.10.1498870955',
+    '__utmc': '17573176',
+    '__utmz': '17573176.1498867554.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)',
+}
+
+# Headers para hacer request
+headers = {
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'es-ES,es;q=0.8,en;q=0.6',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+}
+
+
+with open('usuarios_peseu_2014.csv', 'w', newline='') as file:
+    escritor_csv = csv.writer(file, delimiter=',',
+                              quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    # Iterar sobre  las universidades para conseguir carreras
+    for universidad in universidades_con_carreras:
+        # Iterrar sobre carreras para conseguir postulaciones
+        for carrera in universidades_con_carreras[universidad]:
+            print('[DEBUG] Sacando postulantes de: {}'.format(carrera))
+            # Conseguir la pagina de las postulaciones
+            pagina_postulacion = requests.get('http://www.peseu.com/listas-de-seleccionados/2014/{}/{}'.format(
+                universidad, carrera[1]), headers=headers, cookies=cookies)
+            # Guardar datos de las personas que postulan
+            tree_postulacion = html.fromstring(pagina_postulacion.content)
+            
+            # personas_rut = tree_postulacion.xpath('//tbody/tr/td/text()')[1::5]
+            personas_nombre = tree_postulacion.xpath(
+                '//table/tr/td/text()')[6::4]
+            personas_puntaje = tree_postulacion.xpath(
+                '//table/tr/td/text()')[7::4]
+            personas_situacion = tree_postulacion.xpath(
+                '//table/tr/td/text()')[8::4]
+
+            # Key que se usará para guardar todos estos datos en el diccionario
+            key_a_guardar = carrera[1] + '_' + universidad
+            # Guardar todos los datos obtenidos en el diccionario
+            personas_con_carreras[key_a_guardar] = list(
+                zip(personas_nombre, personas_puntaje, personas_situacion))
+            # Iterar en cada persona que se encontró para guardar en csv
+            for persona in personas_con_carreras[key_a_guardar]:
+                escritor_csv.writerow(persona + (universidad, carrera[0]))
+            
+            print('[DEBUG] Cantidad de personas en: {} = {}'.format(
+                key_a_guardar, len(personas_con_carreras[key_a_guardar])))
